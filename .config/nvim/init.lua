193682f9502719a85dbbf95ef01d996d1ebb7062
Monir <mmonir.m.29@gmail.com>
2026-02-16 23:45:04 +0600

    Add auto completion with TAB

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -198,6 +198,13 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm({ select = true })
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
